5fe69353a82260c3e1dcb2be06da2067e36c6bc0
Yohan Barilleau <barilyoh@gmail.com>
2020-09-08 11:51:59 +0200

    init commit with all the main file

---
+++ tests/Unit/DependencyInjection/ContainerProviderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\Unit\DependencyInjection;
+
+require __DIR__ . "/../../../src/DependencyInjection/ContainerProvider.php";
+
+use PrestaShop\ModuleLibServiceContainer\DependencyInjection\ContainerProvider;
+use PHPUnit\Framework\TestCase;
+
+class ContainerProviderTest extends TestCase
+{
+    // @TODO
+}
---
+++ tests/Unit/DependencyInjection/ServiceContainerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\Unit\DependencyInjection;
+
+require __DIR__ . "/../../../src/DependencyInjection/ServiceContainer.php";
+
+use PrestaShop\ModuleLibServiceContainer\DependencyInjection\ServiceContainer;
+use PHPUnit\Framework\TestCase;
+
+class ServiceContainerTest extends TestCase
+{
+    // @TODO
+}
