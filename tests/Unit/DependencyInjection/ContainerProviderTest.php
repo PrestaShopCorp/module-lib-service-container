7e022d08159ba8c225d23d30a00b3090efffae95
Yohan Barilleau <barilyoh@gmail.com>
2020-09-08 16:36:48 +0200

    update test

--- tests/Unit/DependencyInjection/ContainerProviderTest.php
+++ tests/Unit/DependencyInjection/ContainerProviderTest.php
@@ -20,15 +20,15 @@
 namespace Tests\Unit\DependencyInjection;
 
 use PHPUnit\Framework\TestCase;
-use PrestaShop\ModuleLibServiceContainer\Cache\CacheDirectory;
+use PrestaShop\ModuleLibCacheDirectoryProvider\Cache\CacheDirectoryProvider;
 use PrestaShop\ModuleLibServiceContainer\DependencyInjection\ContainerProvider;
 
 class ContainerProviderTest extends TestCase
 {
     public function testItIsReturnValidInstance()
     {
-        /** @var CacheDirectory $cacheDirectory */
-        $cacheDirectory = $this->createMock(CacheDirectory::class);
+        /** @var CacheDirectoryProvider $cacheDirectory */
+        $cacheDirectory = $this->createMock(CacheDirectoryProvider::class);
         $containerProvider = new ContainerProvider('test', __DIR__, $cacheDirectory);
 
         $this->assertInstanceOf(ContainerProvider::class, $containerProvider);
